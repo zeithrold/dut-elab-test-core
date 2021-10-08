c9ca3d9dd1d805e05ab668420a01b12dff481c2f
Zeithrold <zeithrold@gmail.com>
2021-10-08 18:45:42 +0800

    Deleted HTTP Lib

--- http.h
+++
@@ -1,7 +0,0 @@
-//
-// Created by Zeithrold on 2021/10/1.
-//
-
-namespace dutelab {
-    void start(int port);
-}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include "http.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    dutelab::start(23333);
     return 0;
 }
